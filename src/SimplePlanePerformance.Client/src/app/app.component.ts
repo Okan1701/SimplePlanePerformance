7dcb43f9da0980649d6febcdef07d87ce9ce1668
Okan Emeni <okanemeni@outlook.com>
2025-05-10 13:48:05 +0200

    Adds time clock to navbar

--- src/SimplePlanePerformance.Client/src/app/app.component.ts
+++ src/SimplePlanePerformance.Client/src/app/app.component.ts
@@ -3,6 +3,8 @@ import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { interval, startWith } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +13,15 @@ import {MatButtonModule} from '@angular/material/button';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+	protected localTimeString = "";
+	protected zuluTimeString = "";
+
+	constructor() {
+		interval(1000).pipe(takeUntilDestroyed(), startWith(0)).subscribe(() => {
+			let localDate = new Date();
+			this.localTimeString = `${localDate.getHours()}:${localDate.getMinutes()}:${localDate.getSeconds()} LT`;
+			this.zuluTimeString = `${localDate.getUTCHours()}:${localDate.getUTCMinutes()}:${localDate.getUTCSeconds()} Z`;
+		});
+	}
+
 }
