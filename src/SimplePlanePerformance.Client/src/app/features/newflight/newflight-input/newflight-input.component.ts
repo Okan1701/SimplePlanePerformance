f68a15dbb2b6ec0f1a4b92970d5aa8b85c506f55
Okan Emeni <okanemeni@outlook.com>
2025-05-17 14:57:47 +0200

    Adds more placeholders inputs to new flight page

--- src/SimplePlanePerformance.Client/src/app/features/newflight/newflight-input/newflight-input.component.ts
+++ src/SimplePlanePerformance.Client/src/app/features/newflight/newflight-input/newflight-input.component.ts
@@ -4,10 +4,12 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
 	selector: 'app-newflight-input',
-	imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatIconModule],
+	imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatIconModule, MatSelectModule, MatButtonModule],
 	templateUrl: './newflight-input.component.html',
 	styleUrl: './newflight-input.component.scss'
 })
