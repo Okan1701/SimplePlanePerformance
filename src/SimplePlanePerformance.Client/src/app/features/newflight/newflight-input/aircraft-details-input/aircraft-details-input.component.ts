d1b6b594025d7ab0a3493843df4eaf72886adb52
Okan Emeni <okanemeni@outlook.com>
2025-05-24 14:44:47 +0200

    Adds working aircraft dropdown to input

--- src/SimplePlanePerformance.Client/src/app/features/newflight/newflight-input/aircraft-details-input/aircraft-details-input.component.ts
+++ src/SimplePlanePerformance.Client/src/app/features/newflight/newflight-input/aircraft-details-input/aircraft-details-input.component.ts
@@ -1,17 +1,42 @@
-import { Component } from '@angular/core';
-import { MatCard, MatCardContent, MatCardHeader, MatCardModule, MatCardSubtitle } from "@angular/material/card";
-import { MatInput, MatInputModule, MatLabel } from "@angular/material/input";
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatSelectModule } from '@angular/material/select';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { AircraftsService } from '../../../../shared/services/aircrafts.service';
+import { filter, map, Observable, startWith } from 'rxjs';
+import { Status } from '../../../../shared/enums/status.enum';
+import { CommonModule } from '@angular/common';
+import { Aircraft } from '../../../../shared/models/aircraft.model';
+import { CardInputTitleComponent } from '../card-input-title/card-input-title.component';
 
 @Component({
 	selector: 'app-aircraft-details-input',
-	imports: [MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatDatepickerModule, MatSelectModule],
+	imports: [CommonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatDatepickerModule, MatSelectModule, MatProgressBarModule, CardInputTitleComponent],
 	templateUrl: './aircraft-details-input.component.html',
 	styleUrl: './aircraft-details-input.component.scss'
 })
-export class AircraftDetailsInputComponent {
+export class AircraftDetailsInputComponent implements OnInit {
+	constructor(private aircraftService: AircraftsService) {
+	}
 
+	public ngOnInit() {
+		this.aircraftService.loadAircrafts();
+	}
+
+	protected get isLoading$(): Observable<boolean> {
+		return this.aircraftService.status$.pipe(
+			map(status => status !== Status.Success)
+		);
+	}
+
+	protected get aircraft$(): Observable<Aircraft[]> {
+		return this.aircraftService.aircrafts$.pipe(
+			startWith([]),
+			filter(aircraft => aircraft != null)
+		);
+	}
 }
