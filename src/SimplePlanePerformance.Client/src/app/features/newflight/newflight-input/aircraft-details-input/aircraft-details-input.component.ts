75fbda08bb121830eac15d5b0f1eaed343962bd4
Okan Emeni <okanemeni@outlook.com>
2025-05-25 12:53:08 +0200

    Adds form to aircraft details input component

--- src/SimplePlanePerformance.Client/src/app/features/newflight/newflight-input/aircraft-details-input/aircraft-details-input.component.ts
+++ src/SimplePlanePerformance.Client/src/app/features/newflight/newflight-input/aircraft-details-input/aircraft-details-input.component.ts
@@ -7,20 +7,49 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatSelectModule } from '@angular/material/select';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { AircraftsService } from '../../../../shared/services/aircrafts.service';
-import { filter, map, Observable, startWith } from 'rxjs';
+import {combineLatestWith, filter, map, Observable, startWith} from 'rxjs';
 import { Status } from '../../../../shared/enums/status.enum';
 import { CommonModule } from '@angular/common';
 import { Aircraft } from '../../../../shared/models/aircraft.model';
 import { CardInputTitleComponent } from '../card-input-title/card-input-title.component';
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {FormatAircraftTypePipe} from '../../../../shared/pipes/format-aircraft-type.pipe';
+import {FormatFuelTypePipe} from '../../../../shared/pipes/format-fuel-type.pipe';
 
 @Component({
 	selector: 'app-aircraft-details-input',
-	imports: [CommonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatDatepickerModule, MatSelectModule, MatProgressBarModule, CardInputTitleComponent],
+	imports: [CommonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatDatepickerModule, MatSelectModule, MatProgressBarModule, CardInputTitleComponent, ReactiveFormsModule, MatTooltipModule, FormatAircraftTypePipe, FormatFuelTypePipe],
 	templateUrl: './aircraft-details-input.component.html',
 	styleUrl: './aircraft-details-input.component.scss'
 })
 export class AircraftDetailsInputComponent implements OnInit {
-	constructor(private aircraftService: AircraftsService) {
+	protected selectedAircraft?: Aircraft;
+	protected form: FormGroup<{
+		aircraftId: FormControl<number | null>,
+		registration: FormControl<string | null>
+	}>;
+
+	constructor(formBuilder: FormBuilder, private aircraftService: AircraftsService) {
+		this.form = formBuilder.group({
+			aircraftId: new FormControl(0, [Validators.required]),
+			registration: new FormControl('', [Validators.required])
+		});
+
+		this.form.controls.aircraftId.valueChanges
+			.pipe(
+				takeUntilDestroyed(),
+				combineLatestWith(this.aircraftService.aircrafts$)
+			).subscribe(([id, aircraft]) => {
+				if (id != null && aircraft?.length > 0) {
+					this.selectedAircraft = aircraft.find(x => x.id === id);
+					this.form.controls.registration.setValue(this.selectedAircraft?.registration ?? "");
+				}
+				else {
+					this.selectedAircraft = undefined;
+				}
+		})
 	}
 
 	public ngOnInit() {
