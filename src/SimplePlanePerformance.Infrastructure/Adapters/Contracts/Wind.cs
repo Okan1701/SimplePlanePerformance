f1cb8a7b90476060de2696cef5affcb55bfec1c0
Okan Emeni <okanemeni@outlook.com>
2025-05-30 18:10:31 +0200

    Fixes wind speed not being mapped

--- src/SimplePlanePerformance.Infrastructure/Adapters/Contracts/Wind.cs
+++ src/SimplePlanePerformance.Infrastructure/Adapters/Contracts/Wind.cs
@@ -1,10 +1,21 @@
-﻿namespace SimplePlanePerformance.Infrastructure.Adapters.Contracts;
+﻿using System.Text.Json.Serialization;
+
+namespace SimplePlanePerformance.Infrastructure.Adapters.Contracts;
 
 public class Wind
 {
+    [JsonPropertyName("degrees")]
     public int? Degrees { get; set; }
+    
+    [JsonPropertyName("speed_kph")]
     public int? SpeedKph { get; set; }
+    
+    [JsonPropertyName("speed_kts")]
     public int? SpeedKts { get; set; }
+    
+    [JsonPropertyName("speed_mph")]
     public int? SpeedMph { get; set; }
+    
+    [JsonPropertyName("speed_mps")]
     public int? SpeedMps { get; set; }
 }
