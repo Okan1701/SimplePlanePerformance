4950fbe0b8fc10851d112a5a5d064e5cc961d977
okan <okanemeni@outlook.com>
2025-09-06 14:51:59 +0200

    Re-styles takeoff performance card

--- src/SimplePlanePerformance.Client/src/app/features/newflight/newflight-output/takeoff-performance/takeoff-performance.component.ts
+++ src/SimplePlanePerformance.Client/src/app/features/newflight/newflight-output/takeoff-performance/takeoff-performance.component.ts
@@ -1,14 +1,15 @@
-import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
-import { TakeoffPerformance } from '../../models/takeoff-performance.model';
-import { RunwaySvgComponent } from './runway-svg/runway-svg.component';
+import {Component, Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {TakeoffPerformance} from '../../models/takeoff-performance.model';
+import {RunwaySvgComponent} from './runway-svg/runway-svg.component';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
-  selector: 'app-takeoff-performance',
-	imports: [CommonModule, MatCardModule, RunwaySvgComponent],
-  templateUrl: './takeoff-performance.component.html',
-  styleUrl: './takeoff-performance.component.scss'
+	selector: 'app-takeoff-performance',
+	imports: [CommonModule, MatCardModule, RunwaySvgComponent, MatTooltipModule],
+	templateUrl: './takeoff-performance.component.html',
+	styleUrl: './takeoff-performance.component.scss'
 })
 export class TakeoffPerformanceComponent {
 	@Input()
