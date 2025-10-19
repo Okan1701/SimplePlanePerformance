c6adf6f48bc5a12fc41ce9eda754162f0fb12a45
Okan <okanemeni@outlook.com>
2025-10-19 13:11:49 +0200

    Adds version endpoint

---
+++ src/SimplePlanePerformance.WebAPI/Controllers/VersionController.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using SimplePlanePerformance.WebAPI.ViewModels;
+
+namespace SimplePlanePerformance.WebAPI.Controllers;
+
+[ApiController]
+[Route("/v1/version")]
+public class VersionController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var assembly = Assembly.GetEntryAssembly();
+        if (assembly == null) throw new NullReferenceException("Assembly is null");
+        
+        return Ok(new VersionModel
+        {
+            Major = assembly.GetName().Version?.Major ?? 0,
+            Minor = assembly.GetName().Version?.Minor ?? 0,
+            Build = assembly.GetName().Version?.Build ?? 0,
+        });
+    }
+}
---
+++ src/SimplePlanePerformance.WebAPI/ViewModels/VersionModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SimplePlanePerformance.WebAPI.ViewModels;
+
+public class VersionModel
+{
+    public int Major { get; set; }
+    public int Minor { get; set; }
+    public int Build { get; set; }
+}
